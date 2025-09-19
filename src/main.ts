08e72e0f9b628ed6249476c2d7b8f2f693826a52
VitorMalvestiti <malvestiti.vitor@gmail.com>
2025-09-19 09:03:59 -0300

    feat: add notification module

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.setGlobalPrefix('api');
+  await app.listen(3000);
+  console.log(`http://localhost:3000/api`);
 }
 bootstrap();
