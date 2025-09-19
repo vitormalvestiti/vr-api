0de540cd3a8edbb9a577c8f3e8f884118fd1230c
VitorMalvestiti <malvestiti.vitor@gmail.com>
2025-09-19 09:56:21 -0300

    fix(rabbitmq): adjust service initialization

--- src/notification/rabbitmq.service.ts
+++ src/notification/rabbitmq.service.ts
@@ -3,23 +3,26 @@ import * as amqp from 'amqplib';
 import type { Channel, ChannelModel } from 'amqplib';
 
 @Injectable()
-export class RabbitMQService implements OnModuleInit {
+export class RabbitMQService {
   private connection!: ChannelModel;
   private channel!: Channel;
 
-  async onModuleInit() {
-    this.connection = await amqp.connect(
-      'amqp://bjnuffmq:gj-YQIiEXyfxQxjsZtiYDKeXIT8ppUq7@jaragua-01.lmq.cloudamqp.com'
-    );
-    this.channel = await this.connection.createChannel();
+  async init() {
+    if (!this.connection) {
+      this.connection = await amqp.connect('amqps://bjnuffmq:gj-YQIiEXyfxQxjsZtiYDKeXIT8ppUq7@jaragua-01.lmq.cloudamqp.com/bjnuffmq');
+      this.channel = await this.connection.createChannel();
+      console.log('[RabbitMQ] Canal inicializado');
+    }
   }
 
   async publish(queue: string, message: any) {
+    await this.init();
     await this.channel.assertQueue(queue, { durable: false });
     this.channel.sendToQueue(queue, Buffer.from(JSON.stringify(message)));
   }
 
   async consume(queue: string, callback: (msg: any) => void) {
+    await this.init();
     await this.channel.assertQueue(queue, { durable: false });
     this.channel.consume(queue, (msg) => {
       if (msg) {
@@ -30,4 +33,3 @@ export class RabbitMQService implements OnModuleInit {
     });
   }
 }
-
