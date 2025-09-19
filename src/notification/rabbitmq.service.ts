ebe8a7705ef3a9e6287d439f58cde77ae82aaff7
VitorMalvestiti <malvestiti.vitor@gmail.com>
2025-09-19 10:25:28 -0300

    fix(rabbitmq): handle errors in service

--- src/notification/rabbitmq.service.ts
+++ src/notification/rabbitmq.service.ts
@@ -4,32 +4,41 @@ import type { Channel, ChannelModel } from 'amqplib';
 
 @Injectable()
 export class RabbitMQService {
-  private connection!: ChannelModel;
-  private channel!: Channel;
+    private connection!: ChannelModel;
+    private channel!: Channel;
 
-  async init() {
-    if (!this.connection) {
-      this.connection = await amqp.connect('amqps://bjnuffmq:gj-YQIiEXyfxQxjsZtiYDKeXIT8ppUq7@jaragua-01.lmq.cloudamqp.com/bjnuffmq');
-      this.channel = await this.connection.createChannel();
-      console.log('[RabbitMQ] Canal inicializado');
+    async init() {
+        if (!this.connection) {
+            this.connection = await amqp.connect('amqps://bjnuffmq:gj-YQIiEXyfxQxjsZtiYDKeXIT8ppUq7@jaragua-01.lmq.cloudamqp.com/bjnuffmq');
+            this.channel = await this.connection.createChannel();
+            console.log('[RabbitMQ] Canal inicializado');
+        }
     }
-  }
 
-  async publish(queue: string, message: any) {
-    await this.init();
-    await this.channel.assertQueue(queue, { durable: false });
-    this.channel.sendToQueue(queue, Buffer.from(JSON.stringify(message)));
-  }
+    async publish(queue: string, message: any) {
+        try {
+            await this.init();
+            await this.channel.assertQueue(queue, { durable: false });
+            this.channel.sendToQueue(queue, Buffer.from(JSON.stringify(message)));
+        } catch (err) {
+            console.error(`Erro ao publicar em ${queue}:`, err.message);
+            throw new Error('Erro ao publicar no RabbitMQ');
+        }
+    }
 
-  async consume(queue: string, callback: (msg: any) => void) {
-    await this.init();
-    await this.channel.assertQueue(queue, { durable: false });
-    this.channel.consume(queue, (msg) => {
-      if (msg) {
-        const content = JSON.parse(msg.content.toString());
-        callback(content);
-        this.channel.ack(msg);
-      }
-    });
-  }
+    async consume(queue: string, callback: (msg: any) => void) {
+        try {
+            await this.init();
+            await this.channel.assertQueue(queue, { durable: false });
+            this.channel.consume(queue, (msg) => {
+                if (msg) {
+                    const content = JSON.parse(msg.content.toString());
+                    callback(content);
+                    this.channel.ack(msg);
+                }
+            });
+        } catch (err) {
+            console.error(`Erro ao consumir fila ${queue}:`, err.message);
+        }
+    }
 }
