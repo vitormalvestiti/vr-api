62c3cc7638bc0b09cdd080816cf0549f5d282dca
VitorMalvestiti <malvestiti.vitor@gmail.com>
2025-09-19 10:19:14 -0300

    test(notification): add unit tests for service and controller

--- src/notification/notification.controller.spec.ts
+++ src/notification/notification.controller.spec.ts
@@ -1,18 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { NotificationController } from './notification.controller';
+import { NotificationService } from './notification.service';
 
 describe('NotificationController', () => {
   let controller: NotificationController;
+  let service: NotificationService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [NotificationController],
+      providers: [
+        {
+          provide: NotificationService,
+          useValue: {
+            createNotification: jest.fn(),
+            getStatus: jest.fn().mockReturnValue('AGUARDANDO_PROCESSAMENTO'),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<NotificationController>(NotificationController);
+    service = module.get<NotificationService>(NotificationService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('deve retornar status quando chamar getStatus', () => {
+    const result = controller.getStatus('123');
+    expect(result).toEqual({ mensagemId: '123', status: 'AGUARDANDO_PROCESSAMENTO' });
   });
 });
